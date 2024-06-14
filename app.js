6a5feafa7bdfb50315679b38a1ecc0f18ba4ee15
donnyfauzi <donnyfauzi55@gmail.com>
2024-06-14 20:35:29 +0700

    update nodemon

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log("haiiiii!!!!")
+const express = require("express")
+const app = express()
+const port = 3000
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/profile", (req, res) => {
+  res.send("halaman profile");
+});
+
+app.get("/beranda", (req, res) => {
+  res.send("halaman beranda");
+});
+
+app.listen(port, () => {
+  console.log(` server siap !!!! buka localhost ${port}`);
+});
